1f1e31d7998c16f717903687154c4e8fd09b18e4
Jim Youngquist <jay@cs.uw.edu>
2013-03-13 13:55:16 -0700

    added ADC test code

--- test_code/adc_poll/adc_poll.c
+++ test_code/adc_poll/adc_poll.c
@@ -1,8 +1,11 @@
 #include <msp430.h>
 
+// DOES NOT USE INTERNAL REFERENCE VOLTAGE...which adds about 200uA of drain
+
 void setup(void) {
     // Configuration stuff goes here
-    
+    ADC10CTL0 = SREF_2 | ADC10SHT_0;
+    ADC10CTL1 = INCH_1;         // in channel = A1
 }
 
 int main(void)
@@ -23,16 +26,28 @@ int main(void)
     // Wait for the chip to spin up
     volatile unsigned int i = 1000;
     while(i--);
-    
+
     P1OUT = 0x01;
 
     //////////////////////////////////////////////////
     // TEST CODE GOES HERE
 
+    // turn on the ADC10
+    ADC10CTL0 |= ENC | ADC10ON;
+    volatile unsigned int value;
+    
+    // spin
     i = 2000;
-    while (--i) {
+    while (i--) {
+    // turn on the ADC10
+        if (!(ADC10CTL1 & ADC10BUSY)) {
+            value = ADC10MEM;
+            ADC10CTL0 |= ADC10SC;
+        }
+    };
 
-    }
+    // turn off the ADC10
+    ADC10CTL0 = 0;
     
     //////////////////////////////////////////////////
     
